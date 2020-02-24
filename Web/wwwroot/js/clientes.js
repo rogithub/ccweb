1af440045abd39698396fbf266086ec36b537fce
rodrigo debian <correo.rodrigo@gmail.com>
2020-02-24 15:18:26 -0600

    requirejs

---
+++ Web/wwwroot/js/clientes.js
@@ -0,0 +1,14 @@
+﻿requirejs.config({
+    baseUrl: 'lib',
+        paths: {
+	        jquery: 'jquery/dist/jquery.min',
+		bootstrap: 'bootstrap/dist/js/bootstrap.min',
+		knockout: 'knockout/dist/knockout'
+	}
+});
+
+require(["jquery"], function($) {
+    $(function(){
+	alert("document ready");
+    });
+});
