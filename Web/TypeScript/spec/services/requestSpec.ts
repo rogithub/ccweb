ba2a71310c1860fb549338a3d9baa5d61300b231
rodrigo debian <correo.rodrigo@gmail.com>
2020-02-27 08:50:01 -0600

    adding spec file

---
+++ Web/TypeScript/spec/services/requestSpec.ts
@@ -0,0 +1,16 @@
+import { Request } from '../../services/request';
+
+describe('Component', () => {
+    let service: Request;
+
+    beforeEach(() => {
+        service = new Request();
+    });
+
+    describe('get', () => {
+
+        it("should fetch data", function (done) {
+            done();
+        });
+    });
+});
