bb8719562e723a3f0524b8608904eed35d6d31ab
Rodrigo Debian <rosec@protonmail.ch>
2020-02-29 15:41:04 -0600

    adding coverage

--- Web/TypeScript/spec/components/dataTable/modelSpec.ts
+++ Web/TypeScript/spec/components/dataTable/modelSpec.ts
@@ -1,6 +1,5 @@
 import { Model } from '../../../components/dataTable/model';
 import { DefaultColumn } from '../../../components/dataCell/defaultColumn';
-import { ObjectLiteral } from '../../../shared/objectLiteral';
 import ko from '../../specHelpers/koMock';
 import { ApiMock } from '../../specHelpers/apiMock';
 
@@ -10,12 +9,26 @@ interface Persona {
 }
 
 describe('Model', () => {
+
+    let api: ApiMock;
+    let response = {
+        json: () => [{
+            nombre: "Rodrigo",
+            edad: 37
+        }]
+    }
+
+    beforeEach(() => {
+        api = new ApiMock();
+        api.post = jasmine.createSpy("post").and.resolveTo(response);
+    });
+
     describe('constructor', () => {
 
         it("should init table", (done) => {
 
             let searchUrl = "/search"
-            let m = new Model<Persona>(ko, new ApiMock(), searchUrl, [
+            let m = new Model<Persona>(ko, api, searchUrl, [
                 new DefaultColumn("Nombre"),
                 new DefaultColumn("Edad")
             ]);
