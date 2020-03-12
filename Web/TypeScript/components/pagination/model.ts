c1b165adf577332153af4733f47074862bfd856b
rodrigo debian <correo.rodrigo@gmail.com>
2020-03-12 15:27:03 -0600

    pagination update

--- Web/TypeScript/components/pagination/model.ts
+++ Web/TypeScript/components/pagination/model.ts
@@ -1,31 +1,52 @@
 import range from '../../constants/range';
 
-export class Model {
+export class Pagination {
     private ko: KnockoutStatic;
 
+    public pageSizes: KnockoutObservableArray<number>;
     public pageSize: KnockoutObservable<number>;
     public page: KnockoutObservable<number>;
     public totalRows: KnockoutObservable<number>;
     public list: KnockoutComputed<number[]>;
+    public jumpToPage: KnockoutObservable<string>;
 
-    constructor(ko: KnockoutStatic) {
-        this.ko = ko;
-        this.page = this.ko.observable<number>(1);
-        this.pageSize = this.ko.observable<number>(20);
-        this.totalRows = this.ko.observable<number>(0);
+    constructor(ko: KnockoutStatic, page: number = 1, pageSizes: number[] = [10, 20, 50, 100], totalRows: number = 0) {
+	this.ko = ko;
+	this.page = this.ko.observable<number>(page);
+	this.pageSizes = this.ko.observableArray<number>(pageSizes);
+	this.pageSize = this.ko.observable<number>(pageSizes[0]);
+	this.totalRows = this.ko.observable<number>(totalRows);
+	this.jumpToPage = this.ko.observable<string>(page.toString());
 
-        this.list = this.ko.pureComputed<number[]>(() => {
-            if (this.totalRows() <= 0) return [];
+	const self = this;
+	self.list = self.ko.pureComputed<number[]>(() => {
+	    if (self.totalRows() <= 0) return [];
 
-            let pageCount = this.totalRows() / this.pageSize();
-            let remainder = this.totalRows() % this.pageSize();
+	    let pageCount = self.totalRows() / self.pageSize();
+	    let remainder = self.totalRows() % self.pageSize();
 
-            if (remainder > 0) {
-                pageCount += 1;
-            }
+	    if (remainder > 0) {
+		pageCount += 1;
+	    }
 
-            return range(1, pageCount, 1);
-        });
+	    return range(1, pageCount, 1);
+	}, self);
+
+	this.jumpToPage.subscribe(newPage => {
+	    let tryNewPage = parseInt(newPage);
+	    if (isNaN(tryNewPage) || self.list().indexOf(tryNewPage) === -1 || self.page() === tryNewPage) {
+		return;
+	    }
+
+	    self.page(tryNewPage);
+	});
+
+	this.page.subscribe(newPage => {
+	    self.jumpToPage(newPage.toString());
+	});
+
+	this.pageSize.subscribe(() => {
+	    self.page(1);
+	});
     }
 }
-
